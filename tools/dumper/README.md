8f79e5d457b86198419d6fd3cdb5da7925cc6766
Alexandro Sanchez Bach <alexandro@phi.nz>
2018-03-28 06:05:02 +0200

    Added blobs/debug servers to dumper

--- tools/dumper/README.md
+++ tools/dumper/README.md
@@ -16,3 +16,13 @@ Dumper to dump/extract files required by Orbital from an actual PlayStation 4 co
     ```
 
 4. Enter your computer's IP address in the PlayStation 4 web browser and follow the instructions on screen.
+
+## Development
+
+This dumper requires an exploit that listens for payloads in binary format on port `9020`. These payloads need to be mapped as follows in user address space:
+
+- `0x926200000`: Code (can be changed in `Makefile`)
+- `0x926300000`: Data (can be changed in `Makefile`)
+- `0x926400000`: Arguments (can be changed in `source/main.c`)
+
+Furthermore, the server will listen at port `9021` for incoming blobs, and optionally at `9022` for debug messages.
--- tools/dumper/server.py
+++ tools/dumper/server.py
@@ -1,14 +1,74 @@
 #!/usr/bin/env python3
 
 import argparse
+import binascii
+import socket
+import struct
+import threading
 import os
+import sys
 
 import aiohttp
 from aiohttp import web
 
+# Configuration
+PORT_BLOBS = 9021
+PORT_DEBUG = 9022
+
 # Context
 current_file = None
 
+# Sockets
+def server_blobs():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.bind(('', PORT_BLOBS))
+    except socket.error as msg:
+        print('blobs-server: Bind failed: %s\n' % msg)
+        sys.exit()
+    s.listen(5)
+    while True:
+        c, addr = s.accept()
+        print('blobs-server: Client connected: %s:%s' % addr)
+        while True:
+            file_size = c.recv(8)
+            if not file_size:
+                break
+            file_size = struct.unpack('Q', file_size)[0]
+            if not file_size:
+                break
+            file_hash = c.recv(16)
+            file_data = c.recv(file_size)
+            file_name = 'dump/%s.bin' % file_hash.hex().upper()
+            with open(file_name, 'wb') as f:
+                f.write(file_data)
+        print('blobs-server: Client disconnected: %s:%s' % addr)
+        c.close()
+    s.close()
+
+def server_debug():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.bind(('', PORT_DEBUG))
+    except socket.error as msg:
+        print('debug-server: Bind failed: %s\n' % msg)
+        sys.exit()
+    s.listen(5)
+    while True:
+        c, addr = s.accept()
+        print('debug-server: Client connected: %s:%s' % addr)
+        while True:
+            # TODO: There's surely a better way, but whatever
+            byte = c.recv(1)
+            if not byte:
+                break
+            sys.stdout.buffer.write(byte)
+            sys.stdout.flush()
+        print('debug-server: Client disconnected: %s:%s' % addr)
+        c.close()
+    s.close()
+
+# Website
 async def handle_index(request):
     return web.FileResponse('./index.html')
 
@@ -36,7 +96,13 @@ def main():
         help='Port for HTTP/WS server')
     args = parser.parse_args()
 
-    # Create server
+    # Create sockets
+    t_blobs = threading.Thread(target=server_blobs)
+    t_debug = threading.Thread(target=server_debug)
+    t_blobs.start()
+    t_debug.start()
+    
+    # Create webserver
     app = web.Application()
     app.router.add_get('/', handle_index)
     app.router.add_get('/ws', handle_websocket)
