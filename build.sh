9d1052daee18c073429ec48d6bce366d3d64ac90
Mason Lee Back <masonleeback@gmail.com>
2018-12-16 21:07:51 -0500

    Build process optimizations

--- build.sh
+++ build.sh
@@ -11,39 +11,46 @@ clean="false"
 additional_flags=""
 
 function build_bios() {
-  cd ${path_orbital}
-  cd ${path_bios}
-  make -j$(nproc)
-  mv out/bios.bin ../${path_bin}/ubios.bin
+    cd ${path_orbital}
+    cd ${path_bios}
+    make -j$(nproc)
+    mv out/bios.bin ../${path_bin}/ubios.bin
 }
 
 function build_grub() {
-  # Building
-  cd ${path_orbital}
-  cd ${path_grub}
-  ./autogen.sh
-  ./configure --target=x86_64 --disable-werror
-  make -j$(nproc)
+    cd ${path_orbital}
+    cd ${path_grub}
+    
+    if [ ! -e ./Makefile ]; then
+        ./autogen.sh
+        ./configure --target=x86_64 --disable-werror
+    fi
+    
+    make -j$(nproc)
 }
 
 function build_qemu() {
-  # Building
-  cd ${path_orbital}
-  cd ${path_qemu}
+    cd ${path_orbital}
+    cd ${path_qemu}
 
-  ./configure --target-list=ps4-softmmu \
-    --enable-sdl --enable-vulkan --enable-debug --disable-capstone \
-    --enable-hax ${additional_flags}
+    if [ ! -e ./config-host.mak ]; then
+        ./configure --target-list=ps4-softmmu \
+            --enable-sdl --enable-vulkan --enable-debug --disable-capstone \
+            --enable-hax ${additional_flags}
+    fi
   
-  make -j$(nproc)
+    make -j$(nproc)
 }
 
-function generate_image() {
+function postbuild() {
     cd ${path_orbital}
-    tar -c -f ${path_bin}/memdisk.tar -C resources boot
-    ${path_grub}/grub-mkimage -d ${path_grub}/grub-core \
-        -O i386-pc -o bin/boot.img -m bin/memdisk.tar -c resources/boot/grub/boot.cfg \
-        memdisk biosdisk part_msdos part_gpt gfxterm_menu fat tar bsd memrw configfile
+    if [ $(uname -o) != "Msys" ]; then
+        tar -c -f ${path_bin}/memdisk.tar -C resources boot
+            ${path_grub}/grub-mkimage -d ${path_grub}/grub-core \
+            -O i386-pc -o bin/boot.img -m bin/memdisk.tar -c resources/boot/grub/boot.cfg \
+            memdisk biosdisk part_msdos part_gpt gfxterm_menu fat tar bsd memrw configfile
+    fi
+    yes | cp -f ${path_qemu}/ps4-softmmu/qemu-system-* ${path_bin}/
 }
 
 while [ $# -ne 0 ]
@@ -56,28 +63,28 @@ do
         -disableStackProtector)
         additional_flags+="--disable-stack-protector "
         ;;
-	esac
-	shift
+    esac
+    shift
 done
 
 if [ ${clean} == "true" ]; then
-  echo "Cleaning working directory..."
-  cd ${path_orbital}
-  cd ${path_qemu}
-  make clean
-  if [ $(uname -o) != "Msys" ]; then
+    echo "Cleaning working directory..."
     cd ${path_orbital}
-    cd ${path_bios}
+    cd ${path_qemu}
     make clean
-    cd ${path_orbital}
-    cd ${path_grub}
-    make clean
-  fi
+    if [ $(uname -o) != "Msys" ]; then
+        cd ${path_orbital}
+        cd ${path_bios}
+        make clean
+        cd ${path_orbital}
+        cd ${path_grub}
+        make clean
+    fi
 else
-  build_qemu
-  if [ $(uname -o) != "Msys" ]; then
-    build_bios
-    build_grub
-    generate_image
-  fi
+    build_qemu
+    if [ $(uname -o) != "Msys" ]; then
+        build_bios
+        build_grub
+    fi
+    postbuild
 fi
