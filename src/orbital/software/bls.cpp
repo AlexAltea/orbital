dcec16c9d6dd67b698a3e1d38bb08a8513346849
Alexandro Sanchez Bach <alexandro@phi.nz>
2021-08-19 22:46:22 +0200

    software: Add BLS parser

---
+++ src/orbital/software/bls.cpp
@@ -0,0 +1,88 @@
+/**
+ * BLS format.
+ *
+ * Copyright 2017-2021. Orbital project.
+ * Released under MIT license. Read LICENSE for more details.
+ *
+ * Authors:
+ * - Alexandro Sanchez Bach <alexandro@phi.nz>
+ */
+
+#include "bls.h"
+
+#include <stdexcept>
+
+constexpr U32 BLS_MAGIC = '2BLS';
+constexpr U64 BLS_BLOCK = 0x200;
+
+U64 BlsStream::read(U64 size, void* buffer) {
+	std::unique_lock<std::mutex> lock(bls->mtx);
+	Stream& s = bls->s;
+	s.seek(base + offset, StreamSeek::Set);
+	size = std::min<U32>(size, size - offset);
+	return s.read(size, buffer);
+}
+
+U64 BlsStream::write(U64 size, const void* buffer) {
+	throw std::runtime_error("Unsupported method");
+}
+
+void BlsStream::seek(U64 offset, StreamSeek mode) {
+	switch (mode) {
+	case StreamSeek::Set:
+		this->offset = offset;
+		break;
+	case StreamSeek::Cur:
+		this->offset += offset;
+		break;
+	case StreamSeek::End:
+		this->offset = size - offset;
+		break;
+	default:
+		throw std::runtime_error("Unsupported mode");
+	}
+}
+
+U64 BlsStream::tell() const {
+	return offset;
+}
+
+BlsParser::BlsParser(Stream& s) : s(s) {
+	// Read and verify BLS header
+	s.seek(0, StreamSeek::Set);
+	header = s.read_t<BlsHeader>();
+	assert(header.magic == BLS_MAGIC);
+	assert(header.version == 1);
+}
+
+BlsParser::~BlsParser() {
+}
+
+std::vector<std::string> BlsParser::files() {
+	std::unique_lock<std::mutex> lock(mtx);
+	std::vector<std::string> names(header.num_files);
+	s.seek(sizeof(BlsHeader), StreamSeek::Set);
+	for (auto& name : names) {
+		auto entry = s.read_t<BlsEntry>();
+		name = (char*)&entry.file_name[0];
+	}
+	return names;
+}
+
+BlsStream BlsParser::get(std::string_view name) {
+	std::unique_lock<std::mutex> lock(mtx);
+	s.seek(sizeof(BlsHeader), StreamSeek::Set);
+	for (U32 i = 0; i < header.num_files; i++) {
+		auto entry = s.read_t<BlsEntry>();
+		if (name == (char*)&entry.file_name[0])
+			return BlsStream(this, entry.block_offset * BLS_BLOCK, entry.file_size);
+	}
+	throw std::runtime_error("Could not find file within BLS");
+}
+
+BlsStream BlsParser::get(U32 index) {
+	std::unique_lock<std::mutex> lock(mtx);
+	s.seek(sizeof(BlsHeader) + sizeof(BlsEntry) * index, StreamSeek::Set);
+	auto entry = s.read_t<BlsEntry>();
+	return BlsStream(this, entry.block_offset * BLS_BLOCK, entry.file_size);
+}
---
+++ src/orbital/software/bls.h
@@ -0,0 +1,79 @@
+/**
+ * BLS format.
+ *
+ * Copyright 2017-2021. Orbital project.
+ * Released under MIT license. Read LICENSE for more details.
+ *
+ * Authors:
+ * - Alexandro Sanchez Bach <alexandro@phi.nz>
+ */
+
+#pragma once
+
+#include <core.h>
+
+#include <string>
+#include <string_view>
+#include <vector>
+
+// Forward declarations
+class BlsParser;
+
+struct BlsEntry {
+    LE<U32> block_offset;
+    LE<U32> file_size;
+    LE<U32> padding[2];
+    LE<S08> file_name[32];
+};
+
+struct BlsHeader {
+    LE<U32> magic;
+    LE<U32> version;
+    LE<U32> flags;
+    LE<U32> num_files;
+    LE<U32> num_blocks;
+    LE<U32> padding[3];
+};
+
+class BlsStream : public Stream {
+    BlsParser* bls;
+    const U64 base;
+    const U32 size;
+    U32 offset;
+
+public:
+    BlsStream(BlsParser* bls, U32 base, U32 size)
+        : bls(bls), base(base), size(size), offset(0) {}
+
+    virtual U64 read(U64 size, void* buffer) override;
+    virtual U64 write(U64 size, const void* buffer) override;
+    virtual void seek(U64 offset, StreamSeek mode) override;
+    virtual U64 tell() const override;
+};
+
+class BlsParser {
+    friend BlsStream;
+    Stream& s;
+    BlsHeader header;
+    std::mutex mtx;
+
+public:
+    BlsParser(Stream& s);
+    ~BlsParser();
+
+    /**
+     * Return list of file names corresponding to the BLS entries.
+     * @return  Vector of strings of the file names
+     */
+    std::vector<std::string> files();
+
+    /**
+     * Get BLS stream by file name
+     */
+    BlsStream get(std::string_view name);
+
+    /**
+     * Get BLS stream by index
+     */
+    BlsStream get(U32 index);
+};
