7e4ebf271372f47b806cf81e022f4798f5454e4a
Alexandro Sanchez Bach <alexandro@phi.nz>
2021-08-20 18:21:14 +0200

    ui: Remove old ImGui style key

--- src/orbital/ui/ui_style.cpp
+++ src/orbital/ui/ui_style.cpp
@@ -67,5 +67,4 @@ void ui_style_initialize() {
     style.Colors[ImGuiCol_PlotHistogram] = ImVec4(0.40f, 0.39f, 0.38f, 0.63f);
     style.Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(0.25f, 1.00f, 0.00f, 1.00f);
     style.Colors[ImGuiCol_TextSelectedBg] = ImVec4(0.25f, 1.00f, 0.00f, 0.43f);
-    style.Colors[ImGuiCol_ModalWindowDarkening] = ImVec4(1.00f, 0.98f, 0.95f, 0.73f);
 }
