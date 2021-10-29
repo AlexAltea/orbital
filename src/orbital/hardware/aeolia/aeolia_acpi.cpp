09e173b64c55d7e812dddbe71ccecf5a2aa6cdf8
Alexandro Sanchez Bach <alexandro@phi.nz>
2021-10-29 10:06:39 +0200

    hardware/aeolia: Return version on ACPI function

--- src/orbital/hardware/aeolia/aeolia_acpi.cpp
+++ src/orbital/hardware/aeolia/aeolia_acpi.cpp
@@ -10,6 +10,16 @@
 
 #include "aeolia_acpi.h"
 
+constexpr U32 AEOLIA_V1 = 0x00000100;
+constexpr U32 AEOLIA_V2 = 0x00000200;
+constexpr U32 AEOLIA_V3 = 0x00000300;
+constexpr U32 AEOLIA_V4 = 0x01000100;
+constexpr U32 AEOLIA_V5 = 0x01000200;
+constexpr U32 AEOLIA_V6 = 0x02000100;
+constexpr U32 AEOLIA_V7 = 0x10000100;
+constexpr U32 AEOLIA_V8 = 0x10000200;
+constexpr U32 AEOLIA_V9 = 0x10000201;
+
 AeoliaACPIDevice::AeoliaACPIDevice(PCIeBus* bus, const AeoliaACPIDeviceConfig& config)
     : PCIeDevice(bus, config) {
     // Define BARs
@@ -40,6 +50,10 @@ void AeoliaACPIDevice::reset() {
     header.command = PCI_COMMAND_IO | PCI_COMMAND_MEMORY; // TODO: Is this needed?
     header.header_type |= PCI_HEADER_TYPE_MULTI_FUNCTION;
     header.class_prog = 0x00;
+
+    constexpr U32 aeolia_version = AEOLIA_V3;
+    (U32&)config_data[0x064] = aeolia_version; // HACK: We don't support MSR-based PCIe config space access, so 0x164 becomes 0x64.
+    (U32&)config_data[0x164] = aeolia_version;
 }
 
 U64 AeoliaACPIDevice::mem_read(U64 addr, U64 size) {
